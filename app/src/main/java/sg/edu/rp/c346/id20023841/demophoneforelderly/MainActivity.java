aa9bad5d37e87c710c681966e89517c7a9d6f480
20023841 <20023841@myrp.edu.sg>
2021-05-31 12:01:23 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20023841/demophoneforelderly/MainActivity.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c346.id20023841.demophoneforelderly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnCallSon, btnCallDaughter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCallSon = findViewById(R.id.buttonCallSon);
+        btnCallDaughter = findViewById(R.id.buttonCallDaughter);
+
+        btnCallSon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:+12345678"));
+                startActivity(intentCall);
+            }
+        });
+
+    }
+}
